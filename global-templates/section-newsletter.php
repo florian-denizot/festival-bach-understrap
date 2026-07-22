2a38e2f367151a5333d68e36e59648a5cac32331
Florian Denizot <florian.denizot@gmail.com>
2026-07-21 20:19:41 -0700

    feat: updated the newsletter section style. Added new newsletter image

--- global-templates/section-newsletter.php
+++ global-templates/section-newsletter.php
@@ -6,13 +6,14 @@ $form = get_field('newsletter_form', 'option');
 
 <section id="newsletter" class="py-9 text-bg-primary">
   <div class="container">
-    <div class="row">
+    <div class="row align-items-center">
       
-      <div class="col-md-4">
-        <h2><?php echo $title; ?></h2>
-        <p><?php echo $description; ?></p>   
+      <div class="col-md-5">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/images/FIBM26---iPad-iPhone_450px.png'; ?>" class="img-fluid" alt="Festival Bach">
       </div>
-      <div class="col-md-8">
+      <div class="col-md-7">
+        <h2><?php echo $title; ?></h2>
+        <p><?php echo $description; ?></p>
         <?php echo do_shortcode('[contact-form-7 id="'. $form[0]->ID .'"]'); ?>
       </div>
        
